dcf3563b28c3980cd19646384d17735265af46e4
JuanRdz05 <juanrdz140505@gmail.com>
2026-03-09 00:53:18 -0600

    Modificación en las colisiones de los jugadores con la pelota.

--- Pong/src/PLAYER/player.js
+++ Pong/src/PLAYER/player.js
@@ -2,17 +2,20 @@ import playerConfiguration from "./configuration";
 import k from "../CANVAS/canvas.js";
 
 export function createPlayer(posX, tag) {
+	const isLeft = posX < k.width() / 2;
+
 	return k.add([
 		k.rect(playerConfiguration.width, playerConfiguration.height),
 		k.pos(posX, k.height() / 2 - playerConfiguration.height / 2),
 		k.color("#c99355"),
-		k.area(),
+		k.area({
+			shape: new k.Rect(k.vec2(0), 1, playerConfiguration.height),
+			offset: k.vec2(isLeft ? playerConfiguration.width : 0, 0),
+		}),
 		"player",
 		tag,
 		{
-			//Establecemos la puntuación inicial del jugadro
 			score: 0,
-			//Función para actualizar la puntuación del jugador
 			updateScore() {
 				this.score += 1;
 			},
